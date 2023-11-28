046239319b430f8c7ce3be5e829ffd1d6d3df977
BPelokTUE <b.pelok@student.tue.nl>
2023-11-28 22:27:53 +0100

    results_2.csv

--- parse_game_output.py
+++ parse_game_output.py
@@ -7,35 +7,53 @@ our_agent = "team12_A1"
 # df = pd.DataFrame(columns=["player 1", "player 2", "winner", "time", "board", "score ratio"])
 rows = []
 row = {}
-our_player = 0
 
 with open(f"{file_name}.txt", "r") as f:
     for line in f.readlines():
-        search = re.match(r".*simulate_game.py --first ([^ ]*) --second ([^ ]*) --time ([^ ]*) --board boards/([^ ]*).*", line)
+        search = re.match(r".*play_match.py --first {} --second ([^ ]*) --time ([^ ]*) --board boards/([^ ]*).*".format(our_agent), line)
         if search is not None:
-            row["player 1"] = search.group(1)
-            row["player 2"] = search.group(2)
-
-            our_player = row["player 2"] == our_agent
-
-            row["time"] = search.group(3)
-            row["board"] = search.group(4)
+            row = {}
+            row["player 1"] = our_agent
+            row["player 2"] = search.group(1)
+            row["time"] = search.group(2)
+            row["board"] = search.group(3)
             continue
 
         search = re.match(r".*Score: (\d+) - (\d+).*", line)        
         if search is not None:
-            our_score = int(search.group(our_player + 1))
+            our_score = int(search.group(1))
             total_score = int(search.group(1)) + int(search.group(2))
             row["score ratio"] = round(our_score / total_score, 3)
-            if row["score ratio"] > 0.5:
-                row["winner"] = row[f"player {our_player + 1}"]
-            else:
-                row["winner"] = row[f"player {2 - our_player}"]
             continue
 
-        if row != {}:
-            rows.append(row)
-        row = {}
+        if "player 1" in row:
+            search = re.match(r"^{} - {}.*".format(row["player 1"], row["player 2"]), line)
+            if search is not None:
+                if row["score ratio"] > 0.5:
+                    row["winner"] = row[f"player 1"]
+                elif row["score ratio"] < 0.5:
+                    row["winner"] = row[f"player 2"]
+                else:
+                    row["winner"] = "draw"
+
+                rows.append(row.copy())
+                continue
+
+            search = re.match(r"^{} - {}.*".format(row["player 2"], row["player 1"]), line)
+            if search is not None:
+                row["score ratio"] = 1 - row["score ratio"]
+                row["player 1"], row["player 2"] = row["player 2"], row["player 1"]
+
+                if row["score ratio"] > 0.5:
+                    row["winner"] = row[f"player 2"]
+                elif row["score ratio"] < 0.5:
+                    row["winner"] = row[f"player 1"]
+                else:
+                    row["winner"] = "draw"
+
+                rows.append(row.copy())
+                row["player 1"], row["player 2"] = row["player 2"], row["player 1"]
+                continue
 
 
 pd.DataFrame.from_dict(rows).to_csv(f"{file_name}.csv", index=False)
--- play_games.sh
+++ play_games.sh
@@ -4,7 +4,7 @@
 output_file="game_output.txt"
 
 # Clear the output file
-echo > "${output_file}"
+# echo > "${output_file}"
 
 # Define the time parameters
 time_params=("0.1" "0.5" "1" "5")
@@ -14,28 +14,22 @@ opponent_agents=("random_player" "greedy_player")
 
 # boards=`ls boards`
 # boards=("random-4x4.txt")
-boards=("easy-2x2.txt" "random-2x3.txt" "easy-3x3.txt" "empty-3x3.txt" "hard-3x3.txt" "random-3x3.txt" "random-3x4.txt" "random-4x4.txt")
-num_threads=4
+boards=("random-2x3.txt" "empty-3x3.txt" "random-3x3.txt" "random-3x4.txt" "random-4x4.txt")
 
-
-for i in "${!boards[@]}"; do
-    board=${boards[$i]}
-    n=${num_games[$i]}
-    n2=$((2 * n))
+cnt=0
+for board in "${boards[@]}"; do
     for opponent_agent in "${opponent_agents[@]}"; do
         for time in "${time_params[@]}"; do
-            cnt=0
-            for i in `seq 1 $n`; do
-                command1="simulate_game.py --first ${our_agent} --second ${opponent_agent} --time ${time} --board boards/${board}"
-                command2="simulate_game.py --first ${opponent_agent} --second ${our_agent} --time ${time} --board boards/${board}"
-                for command in "${command1}" "${command2}"; do
-                    cnt=$((cnt + 1))
-                    echo "${command}" >> "${output_file}"
-                    python ${command} | tail -2 >> ${output_file}
-                    echo "Done: ${cnt}/${n2}" >> "$output_file"
-                    echo >> "$output_file"
-                done
-            done
+            if [ $cnt -lt 18 ]; then
+                cnt=$((cnt+1))
+                continue
+            fi
+
+            command="./play_match.py --first ${our_agent} --second ${opponent_agent} --time ${time} --board boards/${board}"
+            echo "${command}" >> "${output_file}"
+            echo "${command}"
+            ${command}  >> "${output_file}"
+            echo "-----------------------------------------" >> "${output_file}"
         done
     done
 done
