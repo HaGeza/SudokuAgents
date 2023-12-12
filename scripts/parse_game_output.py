52cced50a14c5ad1be609343392ec8f4d6f44080
pelokbal <pelokbalazs@gmail.com>
2023-12-12 14:12:27 +0100

    Parse performance logs

--- scripts/parse_game_output.py
+++ scripts/parse_game_output.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import pandas as pd
+import sys
 
 if len(sys.argv) < 3:
     print('Usage: python parse_game_output.py <log_file> <output_file>')
@@ -10,27 +11,46 @@ data = []
 depth_data = []
 fields = ['player1', 'player2', 'winner', 'score1', 'score2', 'board', 'time']
 
+BOARDS_DIR = 'boards/'
+PARAMETERS = {
+    'board': 'empty-2x3',
+    'time': 0.3
+}
+
+for arg in sys.argv[3:]:
+    key, value = arg.split('=')
+    PARAMETERS[key] = value
+
+def reset():
+    # depths, d_ind, scores, winner, board, time
+    return ([[], []], 0, [0, 0], None, PARAMETERS['board'], PARAMETERS['time'])
+
+if len(sys.argv) > 3:
+    for arg in sys.argv[3:]:
+        key, value = arg.split('=')
+        print(key, value)
+
 with open(sys.argv[1], 'r') as f:
     lines = f.readlines()
-    winner = None
-    scores = [0, 0]
-    depths = [[], []]
-    d_ind = 0
-    
-    time = 0.3
-    board = 'empty-2x3'
+    depths, d_ind, scores, winner, board, time = reset()
 
     for line in lines:
-        match = re.match(r'Number of proposals: (\d+)', line)
+        match = re.match(r'.*python.* --time ([^ ]+).* --board ([^ ]+).*', line)        
+
+        if match is not None:
+            time = match.group(1)
+            board = match.group(2).replace(BOARDS_DIR, '')
+
+        match = re.match(r'.*Number of proposals: (\d+).*', line)
         if match is not None:
             depths[d_ind].append(int(match.group(1)))
             d_ind = 1 - d_ind
 
-        match = re.match(r'Score: (\d+) - (\d+)', line)
+        match = re.match(r'.*Score: (\d+) - (\d+).*', line)
         if match is not None:
             scores = [int(match.group(1)), int(match.group(2))]
 
-        match = re.match(r'.*Player (1|2) wins the game.', line)
+        match = re.match(r'.*Player (1|2) wins the game\..*', line)
         if match is not None:
             winner = int(match.group(1))
             continue
@@ -47,9 +67,7 @@ with open(sys.argv[1], 'r') as f:
                     for j in range(len(depths[i])):
                         depth_data.append({'player': player, 'depth': depths[i][j], 'timestamp': 2*j + i})
 
-                depths = [[], []]
-                scores = [0, 0]
-                winner = None
+                depths, d_ind, scores, winner, _, _ = reset()
 
 
 # import json
@@ -72,7 +90,6 @@ print(win_counts)
 #     print(f'{player}: {p2_score} / {p2_total}, ({p2_score / p2_total})')
     
 df.to_csv(sys.argv[2], index=False)
-
 df_depth = pd.DataFrame(depth_data)
 df_depth.to_csv('_depth.'.join(sys.argv[2].rsplit('.', 1)), index=False)
 
