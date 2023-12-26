96ee6cc188dff9e9155e0eff923b1aeb4fd3695c
pelokbal <pelokbalazs@gmail.com>
2023-12-26 13:58:56 +0200

    Simulator done except for encode

--- team12_A3/simulator.py
+++ team12_A3/simulator.py
@@ -37,7 +37,7 @@ class GameStateDict:
     def _get_dict(self, properties: dict):
         items = list(properties.items())
         if len(items) == 0:
-            return (0, 0)
+            return [0, 0]
 
         rest = dict(items[1:])
         rest_dict = self.get_dict(rest)
@@ -52,18 +52,19 @@ class GameStateDict:
         self.root = [vals[0] for vals in properties.values()]
 
 
-    def get(self, keys) -> (int, int):
+    def get(self, keys: list) -> (int, int):
         value = self.dict
         for key in keys:
             value = value[key]
         return value
 
     
-    def set(self, keys, new_value):
+    def update(self, keys: list, q: int):
         value = self.dict
         for key in keys:
             value = value[key]
-        value[keys] = new_value
+        value[0] += 1
+        value[1] += q
 
 
 class BoardState:
@@ -78,6 +79,10 @@ class BoardState:
         self.available = np.ones((self.N, self.N, self.N), dtype=bool)
 
 
+    def __get_item__(self, i: int, j: int) -> int:
+        return self.board[i, j]
+
+
     def _get_block_boundaries(self, i: int, j: int) -> (int, int, int, int):
         block_i = i // self.m
         block_j = j // self.n
@@ -90,7 +95,7 @@ class BoardState:
 
 
     def apply_move(self, i: int, j: int, value: int) -> BoardState:
-        self.board[i, j] = value
+        self[i, j] = value
 
         top, bottom, left, right = self._get_block_boundaries(i, j)
         # No move is available in cell (i,j) anymore
@@ -115,43 +120,80 @@ class BoardState:
         self.available = np.ones((self.N, self.N, self.N), dtype=bool)
 
 
-def simulate_games(num_games: int = 3):
-    game_state_dict = GameStateDict({
-        'player': [0, 1],
-        'parity': [0, 1],
-        'points': [0, 1, 3, 7],
-        'total_parity': list(range(0, 3 * 16 + 1)),
-        'total_missing': list(range(0, 3 * 16 * (16 + 1) + 1))
-    })
+class Simulator:
+    def __init__(self, gsd: GameStateDict, m: int, n: int):
+        self.gsd = gsd
+        self.bs = BoardState(m, n)
+        self.scores = [0, 0]
 
-    board_state = BoardState()
 
-    def do_next_move(gs: list, bs: BoardState, gsd: GameStateDict) -> (int, int):
-        if gsd.get(gs) != (0, 0):
-            moves = bs.get_possible_moves()
+    def _encode(self, parent_gs: list) -> list:
+        return []
 
-            states = []
+
+    def _uct(self, parent_gs: list, child_gs, C: int = 2) -> float:
+        n_parent, _ = gsd.get(parent_gs)
+        n_child, q_child = gsd.get(child_gs)
+        return q_child / n_child + C * np.sqrt(np.log(n_parent) / n_child)
+
+
+    def _do_next_move(self, parent_gs: list) -> int:
+        missing_ind = self.gsd.order.index('total_missing')
+        if parent_gs[missing_ind] == 0:
+            q = 1 if self.scores[0] > self.scores[1] else -1 if self.scores[0] < self.scores[1] else 0
+            self.gsd.update(parent_gs, q)
+            return q
+
+        moves = self.bs.get_possible_moves()
+
+        if self.gsd.get(parent_gs) != (0, 0):
+            child_gss = []
             for i, j, value in moves:
-                bs.board[i, j] = value
-                states.append(encode(gs, bs))
-                bs.board[i, j] = 0
+                self.bs[i, j] = value
+                child_gss.append(self._encode(parent_gs))
+                self.bs[i, j] = 0
 
-            move_ind = np.argmax([uct(state) for state in states])
+            move_ind = np.argmax([self._uct(parent_gs, child_gs) for child_gs in child_gss])
             i, j, value = moves[move_ind]
-            bs.board[i, j] = value
-            gsd.set(states[move_ind], do_next_move(states[move_ind], bs, gsd))
+            self.bs[i, j] = value
+            state = child_gss[move_ind]
+        else:
+            i, j, value = np.random.choice(moves)
+            self.bs[i, j] = value
+            state = self._encode(parent_gs)
+
+        points_ind = self.gsd.order.index('points')
+        player_ind = self.gsd.order.index('player')
+        player = parent_gs[player_ind]
+
+        self.scores[player] += state[points_ind]
+        q = self._do_next_move(state)
+        self.gsd.update(state, q)
+        return q
+
+
+    def simulate_games(self, num_games: int = 3):
+        for _ in range(num_games):
+            gs = gsd.root
+            # select random starting player
+            player_ind = gsd.order.index('player')
+            gs[player_ind] = np.random.choice([0, 1])
+
+            self.bs.reset()
+            self.scores = [0, 0]
+
+            gsd.update(gs, self._do_next_move(gs))
+
+
         
-
-    for _ in range(num_games):
-        game_state = game_state_dict.root
-        # select random starting player
-        game_state[0] = np.random.choice([0, 1])
-        board_state.reset()
-
-        game_state_dict.set(game_state, do_next_move(game_state, board_state, game_state_dict))
-
-
-        
+bs = BoardState()
+gsd = GameStateDict({
+    'player': [0, 1],
+    'parity': [0, 1],
+    'points': [0, 1, 3, 7],
+    'total_parity': list(range(0, 3 * 16 + 1)),
+    'total_missing': list(range(0, 3 * 16 * (16 + 1) + 1))
+})
 
 
 
