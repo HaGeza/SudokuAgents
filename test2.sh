b05eae8ac257e1ea4ce539d83a230c3903291ea2
BPelokTUe <b.pelok@student.tue.nl>
2024-01-04 17:17:19 +0200

    MCTS tests

---
+++ test2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "python play_match.py --first team12_A3_MCTS --second random_player --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second random_player --time 0.5 --board boards/empty-3x3.txt
+echo "python play_match.py --first team12_A3_MCTS --second random_player --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second random_player --time 0.5 --board boards/empty-3x3.txt
+echo "python play_match.py --first team12_A3_MCTS --second random_player --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second random_player --time 0.5 --board boards/empty-3x3.txt
+
+echo "python play_match.py --first team12_A3_MCTS --second greedy_player --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second greedy_player --time 0.5 --board boards/empty-3x3.txt
+echo "python play_match.py --first team12_A3_MCTS --second greedy_player --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second greedy_player --time 0.5 --board boards/empty-3x3.txt
+echo "python play_match.py --first team12_A3_MCTS --second greedy_player --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second greedy_player --time 0.5 --board boards/empty-3x3.txt
+
+echo "python play_match.py --first team12_A3_MCTS --second team12_A2 --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second team12_A2 --time 0.5 --board boards/empty-3x3.txt
+echo "python play_match.py --first team12_A3_MCTS --second team12_A2 --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second team12_A2 --time 0.5 --board boards/empty-3x3.txt
+echo "python play_match.py --first team12_A3_MCTS --second team12_A2 --time 0.5 --board boards/empty-3x3.txt"
+python play_match.py --first team12_A3_MCTS --second team12_A2 --time 0.5 --board boards/empty-3x3.txt
+
